402a9d22e0333a546aa653ad2a1a551a43529b65
dohyun <wook118@naver.com>
2023-04-24 18:02:25 +0900

    0424

--- HelloApp.sql
+++ HelloApp.sql
@@ -42,3 +42,60 @@ values('admin@email.com', '1234', '관리자', '010-5555-5555','Admin');
 commit;
 
 select * from members;
+
+
+
+
+select * from tab;
+
+select * from tbl_notice;
+
+delete from tbl_notice where notice_no > 100;
+
+commit;
+
+create table tbl_reply(
+reply_no number primary key,
+notice_no number not null,
+reply varchar2(1000)not null,--댓글 내용
+reply_writer varchar2(50) not null,
+reply_date date default sysdate
+);
+
+create sequence reply_seq;
+
+insert into tbl_reply
+values(reply_seq.nextval ,98,'98번 공지사항의 댓글입니다','user01',sysdate);
+
+insert into tbl_reply
+values(reply_seq.nextval ,98,'98번 공지사항의 댓글입니다','user02',sysdate);
+
+insert into tbl_reply
+values(reply_seq.nextval ,98,'98번 공지사항의 댓글입니다','user03',sysdate);
+
+
+commit;
+
+select * from tbl_reply
+where notice_no = 100;
+
+delete from tbl_reply where reply_no > 15;
+
+select * from members;
+
+insert into members (email, password, name, phone, auth)
+values('User1@email.com', '1234', '홍길동', '010-1111-1111','User');
+insert into members (email, password, name, phone, auth)
+values('User2@email.com', '1234', '이길동', '010-2222-2222','User');
+insert into members (email, password, name, phone, auth)
+values('User3@email.com', '1234', '박길동', '010-3333-3333','User');
+insert into members (email, password, name, phone, auth)
+values('User4@email.com', '1234', '최길동', '010-4444-4444','User');
+insert into members (email, password, name, phone, auth)
+values('admin@email.com', '1234', '관리자', '010-5555-5555','Admin');
+
+
+
+
+
+
